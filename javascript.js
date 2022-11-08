0831da890c6eae3cb4279fa75ae9c84035a40f73
sina <srezaeihaddad@gmail.com>
2022-11-07 22:47:43 -0500

    Fix PlayRound Function

--- javascript.js
+++ javascript.js
@@ -5,16 +5,20 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection.toLowerCase()==="rock" && computerSelection==="paper" || playerSelection.toLowerCase()==="paper" 
-    && computerSelection==="scissors" || playerSelection.toLowerCase()==="scissors" && computerSelection==="rock") {
-        return(`You lose! ${computerSelection} beats ${playerSelection}`)
-    } 
-    else if (playerSelection.toLowerCase()==="paper" && computerSelection==="rock" || playerSelection.toLowerCase()==="rock" 
-    && computerSelection==="scissors" || playerSelection.toLowerCase()==="scissors" && computerSelection==="paper") {
-        return(`You win! ${playerSelection} beats ${computerSelection}`)
-    } 
-    else {
-        return ("It's a tie!");
+    if (choices.indexOf(playerSelection.toLowerCase()) >= 0) {
+        if (playerSelection.toLowerCase()==="rock" && computerSelection==="paper" || playerSelection.toLowerCase()==="paper" 
+        && computerSelection==="scissors" || playerSelection.toLowerCase()==="scissors" && computerSelection==="rock") {
+            return(`You lose! ${computerSelection} beats ${playerSelection}`)
+        } else if (playerSelection.toLowerCase()==="paper" && computerSelection==="rock" || playerSelection.toLowerCase()==="rock" 
+        && computerSelection==="scissors" || playerSelection.toLowerCase()==="scissors" && computerSelection==="paper") {
+            return(`You win! ${playerSelection} beats ${computerSelection}`)
+        } else {
+            return ("It's a tie!");
+        }
+    } else {
+        const playerSelection = prompt("Rock, Paper, or Scissors?", "Rock");
+        alert("Wrong input! Try again.")
+        return (playRound(playerSelection, computerSelection));
     }
 }
 
