30925df0a1b511a929f80c689f56dbb556333825
sina <srezaeihaddad@gmail.com>
2022-11-07 20:56:16 -0500

    Add Game Function

--- javascript.js
+++ javascript.js
@@ -1,5 +1,24 @@
+let choices = ["rock", "paper", "scissors"];
+
 function getComputerChoice() {
-    let choices = ["Rock", "Paper", "Scissors"];
     return choices[Math.floor(Math.random()*choices.length)];
 }
-console.log(getComputerChoice())
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase()==="rock" && computerSelection==="paper" || playerSelection.toLowerCase()==="paper" 
+    && computerSelection==="scissors" || playerSelection.toLowerCase()==="scissors" && computerSelection==="rock") {
+        return(`You lose! ${computerSelection} beats ${playerSelection}`)
+    } 
+    else if (playerSelection.toLowerCase()==="paper" && computerSelection==="rock" || playerSelection.toLowerCase()==="rock" 
+    && computerSelection==="scissors" || playerSelection.toLowerCase()==="scissors" && computerSelection==="paper") {
+        return(`You win! ${playerSelection} beats ${computerSelection}`)
+    } 
+    else {
+        return ("It's a tie!");
+    }
+}
+
+const playerSelection = prompt("Rock, Paper, or Scissors?", "Rock");
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
