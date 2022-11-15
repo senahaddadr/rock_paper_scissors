9e5e9ee3339cbd46455cb13ef33f7c994511d63a
sina <srezaeihaddad@gmail.com>
2022-11-15 11:50:10 -0500

    Fix User Buttons

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-let choices = ["rock", "paper", "scissors"];
+let choices = ["rock", "paper", "scissors"];    
 
 function getComputerChoice() {
     return choices[Math.floor(Math.random()*choices.length)];
@@ -8,26 +8,31 @@ function playRound(playerSelection, computerSelection) {
     if (choices.indexOf(playerSelection.toLowerCase()) >= 0) {
         if (playerSelection.toLowerCase()==="rock" && computerSelection==="paper" || playerSelection.toLowerCase()==="paper" 
         && computerSelection==="scissors" || playerSelection.toLowerCase()==="scissors" && computerSelection==="rock") {
-            return(`You lose! ${computerSelection} beats ${playerSelection}`)
+            return(`You lose! ${computerSelection} beats ${playerSelection}`);
         } else if (playerSelection.toLowerCase()==="paper" && computerSelection==="rock" || playerSelection.toLowerCase()==="rock" 
         && computerSelection==="scissors" || playerSelection.toLowerCase()==="scissors" && computerSelection==="paper") {
-            return(`You win! ${playerSelection} beats ${computerSelection}`)
+            return(`You win! ${playerSelection} beats ${computerSelection}`);
         } else {
             return ("It's a tie!");
         }
     } else {
         const playerSelection = prompt("Rock, Paper, or Scissors?", "Rock");
-        alert("Wrong input! Try again.")
+        alert("Wrong input! Try again.");
         return (playRound(playerSelection, computerSelection));
     }
-}
+}  
 
-function game() {
-    for (i=1; i<5; i++) {
-        const playerSelection = prompt("Rock, Paper, or Scissors?", "Rock");
-        const computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection, computerSelection));
-    }
-}
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener ('click', () => {
+        console.log(playRound(button.id, getComputerChoice()))});
+});
 
-game()
+// function game() {
+//     for (i=1; i<5; i++) {
+//         const playerSelection = prompt("Rock, Paper, or Scissors?", "Rock");
+//         console.log(playRound(playerSelection, computerSelection));
+//     }
+// }
+
+// game()
